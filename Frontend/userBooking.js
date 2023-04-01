58acfa2dcdc47a814f9683590feedcf7a974fc2c
Krishna-7777 <kchhabraji13@gmail.com>
2023-04-01 20:34:56 +0530

    User Bookings Page

--- Frontend/userBooking.js
+++ Frontend/userBooking.js
@@ -1,24 +1,30 @@
-(async()=>{
-    let data= await (await fetch(`http://127.0.0.1:4500/avail/userBookings/${localStorage.getItem("name")}`,{
-    headers:{
-      "Authorization":localStorage.getItem("token")
-    }
-  })).json();
-  console.log(data);
-  let Html="";
-  if(data.length){
-    data.map((b)=>{
-    Html+=`
-    <div>
-    <h2>Lawyer : ${b.lawyername}</h2>
-    <h2>Time : ${b.time}</h2>
-    <h2>Date : ${b.date}</h2>
+(async () => {
+    let data = await (await fetch(`http://127.0.0.1:4500/avail/userBookings/${localStorage.getItem("name")}`, {
+        headers: {
+            "Authorization": localStorage.getItem("token")
+        }
+    })).json();
+    console.log(data);
+    let Html = "";
+    if (data.length) {
+        data.map((b) => {
+            let date = b.date.split(' ')
+            date.pop()
+            date.pop()
+            date.pop()
+            date.pop()
+            date.pop()
+            Html += `
+<div>
+<h2>Client : ${b.username}</h2>
+<h2>Time : ${b.time}</h2>
+<h2>Date : ${date.join(' ')}</h2>
     <h2>Description : ${b.description}</h2>
     </div>
     <hr>
     `})
-  }else{
-    Html="No Bookings Found"
-  }
-    document.getElementById("bookingList").innerHTML=Html
+    } else {
+        Html = "<h2>No Bookings Found</h2>"
+    }
+    document.getElementById("bookingList").innerHTML = Html
 })()
